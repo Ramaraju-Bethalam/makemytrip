50340cb541149d2d4fc5d8435e6e88315543c3b0
Ramaraju-Bethalam <lb4170@srmist.edu.in>
2025-10-25 00:16:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchButton = document.querySelector(".search-box button");
+    
+    searchButton.addEventListener("click", () => {
+        alert("Search functionality will be implemented soon!");
+    });
+});
